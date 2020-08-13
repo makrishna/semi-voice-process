5306c130e1f188969891929abe6651d52a49f664
makrishna <makrishna@anutanetworks.com>
2020-08-13 23:25:49 +0530

    fix

--- main.js
+++ main.js
@@ -52,9 +52,62 @@ jQuery(function(){
     $('li').removeClass();
     $(this).parent().addClass('mm-active');
 });
-
+let adFile;
 window.addEventListener('DOMContentLoaded', (event) => {
-	let audio = new Audio('3.Rebuttals\COD.wav');
+	let audio = new Audio('./3.Rebuttals/COD.wav');
 	audio.preload;
-    console.log('DOM fully loaded and parsed');
+	console.log('DOM fully loaded and parsed');
+	let myRequest = new XMLHttpRequest();
+	myRequest.open('GET','./3.Rebuttals/COD.wav');
+	myRequest.responseType = "arraybuffer"; // Read as Binary Data
+	myRequest.onload = function() {
+		context.decodeAudioData(myRequest.response, function(buffer){
+			electro = buffer; // Decode the Audio Data and Store it in a Variable
+		});
+	}
+	myRequest.onreadystatechange = function () { 
+		// 4. check if the request has a readyState of 4, which indicates the server has responded (complete)
+		if (myRequest.readyState === 4) {
+			// 5. insert the text sent by the server into the HTML of the 'ajax-content'
+			console.log(myRequest.response);
+			myRequest.response.play();
+			adFile = new Audio(myRequest.response);
+			adFile.setsr
+			
+		}
+	};
+	myRequest.send();
 });
+
+(function(){
+	
+	var context = new AudioContext(); // Create and Initialize the Audio Context
+	var electro; // Create the Sound 
+	var getSound = new XMLHttpRequest(); // Load the Sound with XMLHttpRequest
+	getSound.open("GET", "./3.Rebuttals/COD.wav", true); // Path to Audio File
+	getSound.responseType = "arraybuffer"; // Read as Binary Data
+	getSound.onload = function() {
+		context.decodeAudioData(getSound.response, function(buffer){
+			electro = buffer; // Decode the Audio Data and Store it in a Variable
+		});
+	}
+	getSound.send(); // Send the Request and Load the File
+	
+	window.addEventListener("keydown",onKeyDown); // Create Event Listener for KeyDown
+	
+	function onKeyDown(e){
+		switch (e.keyCode) {
+			// X
+			case 88:
+				var playSound = context.createBufferSource(); // Declare a New Sound
+				playSound.buffer = electro; // Attatch our Audio Data as it's Buffer
+				playSound.connect(context.destination);  // Link the Sound to the Output
+				playSound.start(0); // Play the Sound Immediately
+			break;
+		}
+ 	}
+}());
+
+function play(){
+	adFile.play();
+}
