485e9a2e1fbc6550ef8b8a85f8e14f8e95f7c81c
makrishna <makrishna@anutanetworks.com>
2020-08-13 21:41:06 +0530

    first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,53 @@
+var jukeBox=document.querySelector(".player")
+jukeBox.addEventListener("click",function (event){
+	var songName=event.target.getAttribute("data-src")
+	var audioPlayer=document.querySelector("#player");
+	var audioFile = new Audio(songName);
+	if(audioPlayer){
+		if(songName === audioPlayer.getAttribute("src")){
+			if(audioPlayer.paused){
+				audioPlayer.play();
+				event.target.id="playing"
+			}else{
+				audioPlayer.pause()
+				event.target.id="paused"
+			}
+		}
+		else{
+			audioPlayer.src=songName;
+			audioPlayer.play()
+			if (document.querySelector('#playing')) {
+                document.querySelector('#playing').id = '';
+              }
+            else {
+                document.querySelector('#paused').id = '';
+                
+            }
+            event.target.id = 'playing';
+
+		}
+	}
+	else{
+		var audioPlayer=document.createElement("audio");
+		audioPlayer.id="player";
+		document.body.appendChild(audioPlayer);
+		audioPlayer.src=songName;
+		audioPlayer.play()
+		event.target.id = 'playing';
+	}
+})
+
+$('button').on('click', function(){
+    $('button').removeClass('selected');
+    $(this).addClass('selected');
+});
+jQuery(function(){
+        
+        jQuery('.showSingle').click(function(){
+              jQuery('.targetDiv').hide();
+              jQuery('#div'+$(this).attr('target')).show();
+        });
+});$('li > a').click(function() {
+    $('li').removeClass();
+    $(this).parent().addClass('mm-active');
+});
