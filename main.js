da238a25cfd992a1d0fc402f02d80d6025833921
makrishna <makrishna@anutanetworks.com>
2020-08-14 23:23:41 +0530

    layout fixes

--- main.js
+++ main.js
@@ -1,76 +1,54 @@
 let jukeBox = document.querySelector(".player");
 let context = new AudioContext(); // Create and Initialize the Audio Context
 let namesAndSounds = new Map();
+let divs = ['div1', 'div4', 'div5'];
+let isFinished = false;
 
 window.addEventListener('DOMContentLoaded', (event) => {
-	let intro = document.getElementById('div1');
-	let buttons = intro.getElementsByClassName('button');
-	for (let i = 0; i < buttons.length; i++) {
-		setAudioToButton(buttons[i]);
-	}
+	divs.forEach(ele => {
+		loadSounds(ele);
+	})
 	localStorage.clear();
 });
 
 
 (function () {
 	window.addEventListener("keydown", onKeyDown); // Create Event Listener for KeyDown
-	function onKeyDown(e){
+	function onKeyDown(e) {
 		switch (e.keyCode) {
 			// X
 			case 88:
-				var playSound = context.createBufferSource(); // Declare a New Sound
-				playSound.buffer = electro; // Attatch our Audio Data as it's Buffer
-				playSound.connect(context.destination);  // Link the Sound to the Output
-				playSound.start(0); // Play the Sound Immediately
-			break;
+				console.log("dsfdsf");
+				break;
 		}
- 	}
+	}
 }());
 
-function stopAll(){
+function stopAll() {
 	context.close();
 }
-
+let elem = document.getElementById("myBar");
+let width = 0;
 function loadSounds(id) {
-	let intro = document.getElementById(id);
-	let buttons = intro.getElementsByClassName('button');
+	let btnElement = document.getElementById(id);
+	let buttons = btnElement.getElementsByTagName("button");
 	if (buttons.length) {
 		for (let i = 0; i < buttons.length; i++) {
-			setAudioToButton(buttons[i]);
+			setAudioToButton(buttons[i], buttons.length);
 		}
-	}
-	else {
-		buttons = intro.getElementsByClassName('buttonrebtl');
-	}
-
-	if (buttons.length) {
-		for (let i = 0; i < buttons.length; i++) {
-			setAudioToButton(buttons[i]);
-		}
-	}
-	else {
-		buttons = intro.getElementsByClassName('buttonbntr');
-	}
-
-	if (buttons.length) {
-		for (let i = 0; i < buttons.length; i++) {
-			setAudioToButton(buttons[i]);
-		}
-	}
-	else {
-		return;
 	}
 	localStorage.clear();
 }
 
-function setAudioToButton(element) {
+function setAudioToButton(element, length) {
 	var context = new AudioContext();
 	let getSound = new XMLHttpRequest(); // Load the Sound with XMLHttpRequest
 	getSound.open("GET", "./" + element.dataset.src, true); // Path to Audio File
 	getSound.responseType = "arraybuffer"; // Read as Binary Data
 	getSound.onload = function () {
 		context.decodeAudioData(getSound.response, function (buffer) {
-			namesAndSounds.set(element.dataset.src,buffer);
+			namesAndSounds.set(element.dataset.src, buffer);
+			elem.style.width = (100 / length) * 84 + '%';
 		});
 	}
 	getSound.send();
@@ -88,42 +66,51 @@ jukeBox.addEventListener("click", function (event) {
 				context.resume()
 			}
 			localStorage.setItem('songName', songName);
+			if (isFinished) {
+				playNewSong(songName, electro);
+			}
 		}
 		else {
-			playNewSong(songName,electro)
+			playNewSong(songName, electro)
 		}
 	}
 	else {
-		playNewSong(songName,electro);
+		playNewSong(songName, electro);
 	}
 })
 
-function playNewSong(songName,electro){
+function playNewSong(songName, electro) {
 	if (context.state === 'running' || context.state === 'suspended') {
 		context.close();
 	}
 	context = new AudioContext();
-	let playSound = context.createBufferSource(); // Declare a New Sound
-	playSound.buffer = electro; // Attatch our Audio Data as it's Buffer
-	playSound.connect(context.destination);  // Link the Sound to the Output
-	playSound.start(0); // Play the Sound Immediately
+	let playSound = context.createBufferSource();
+	playSound.buffer = electro; 
+	playSound.connect(context.destination); 
+	playSound.start(0);
 	event.target.id = 'playing';
 	localStorage.setItem('songName', songName);
+	playSound.onended = onEnded;
 }
 
-$('button').on('click', function(){
-    $('button').removeClass('selected');
-    $(this).addClass('selected');
+function onEnded() {
+	isFinished = true;
+	console.log('playback finished');
+}
+
+$('button').on('click', function () {
+	$('button').removeClass('selected');
+	$(this).addClass('selected');
 });
-jQuery(function(){
-        
-        jQuery('.showSingle').click(function(){
-              jQuery('.targetDiv').hide();
-              jQuery('#div'+$(this).attr('target')).show();
-        });
-});$('li > a').click(function() {
-    $('li').removeClass();
-    $(this).parent().addClass('mm-active');
+jQuery(function () {
+
+	jQuery('.showSingle').click(function () {
+		jQuery('.targetDiv').hide();
+		jQuery('#div' + $(this).attr('target')).show();
+	});
+}); $('li > a').click(function () {
+	$('li').removeClass();
+	$(this).parent().addClass('mm-active');
 });
 
 
